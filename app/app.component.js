61812047f2ea28275230de6d6751efb12ed6b114
jaideng123 <jaideng123@yahoo.com>
2016-02-06 18:56:50 -0600

    rough draft

--- app/app.component.js
+++ app/app.component.js
@@ -22,9 +22,65 @@ System.register(['angular2/core', 'angular2/http'], function(exports_1) {
             AppComponent = (function () {
                 function AppComponent(http) {
                     var _this = this;
+                    var ctrl = this;
+                    this.people = [];
+                    this.currentPeople = [];
                     http.get('js/people.json')
                         .map(function (res) { return res.json(); })
                         .subscribe(function (people) { return _this.people = people; });
+                    var isNow = function (begin, end) {
+                        var currentDay = new Date();
+                        if ((currentDay.getHours() < (Number(begin[0]) - 1)))
+                            return false;
+                        if ((currentDay.getHours() == (Number(begin[0]) - 1)) && (currentDay.getMinutes() < (Number(begin[1]) - 1)))
+                            return false;
+                        if ((currentDay.getHours() > (Number(end[0]) - 1)))
+                            return false;
+                        if ((currentDay.getHours() == (Number(end[0]) - 1)) && (currentDay.getMinutes() > (Number(end[1]) - 1)))
+                            return false;
+                        return true;
+                    };
+                    var isCorrectDay = function (days) {
+                        var currentDay = new Date();
+                        for (var i = days.length - 1; i >= 0; i--) {
+                            if (days[i] == 'S' && currentDay.getDay() == 0)
+                                return true;
+                            if (days[i] == 'M' && currentDay.getDay() == 1)
+                                return true;
+                            if (days[i] == 'T' && currentDay.getDay() == 2)
+                                return true;
+                            if (days[i] == 'W' && currentDay.getDay() == 3)
+                                return true;
+                            if (days[i] == 'R' && currentDay.getDay() == 4)
+                                return true;
+                            if (days[i] == 'F' && currentDay.getDay() == 5)
+                                return true;
+                        }
+                        return false;
+                    };
+                    setInterval(function () {
+                        var newPeople = [];
+                        for (var i = ctrl.people.length - 1; i >= 0; i--) {
+                            for (var j = ctrl.people[i].hours.length - 1; j >= 0; j--) {
+                                var normalized = ctrl.people[i].hours[j].time.replace(' ', '').replace('pm', '').replace('am', '').split('(')[0];
+                                var correctTime = false;
+                                for (var k = normalized.split(',').length - 1; k >= 0; k--) {
+                                    var time = normalized.split(',')[k].split('-');
+                                    var begin = time[0].split(':');
+                                    var end = time[1].split(':');
+                                    //convert to military time
+                                    if (Number(begin[0]) < 9)
+                                        begin[0] = (Number(begin[0]) + 12).toString();
+                                    if (Number(end[0]) < 9)
+                                        end[0] = (Number(end[0]) + 12).toString();
+                                    correctTime = correctTime || (isNow(begin, end) && isCorrectDay(ctrl.people[i].hours[j].day.split('')));
+                                }
+                                if (correctTime)
+                                    newPeople.push(ctrl.people[i]);
+                            }
+                            ctrl.currentPeople = newPeople;
+                        }
+                    }, 3000);
                 }
                 AppComponent = __decorate([
                     core_1.Component({
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,5 @@
 import {Component} from 'angular2/core';
 import {Http, HTTP_PROVIDERS} from 'angular2/http';
-
 @Component({
     selector: 'my-app',
     viewProviders: [HTTP_PROVIDERS],
@@ -8,11 +7,68 @@ import {Http, HTTP_PROVIDERS} from 'angular2/http';
 })
 export class AppComponent { 
 	constructor(http: Http) {
-       http.get('js/people.json')
+	   var ctrl = this;
+	   this.people = [];
+	   this.currentPeople = [];
+   	    http.get('js/people.json')
       // Call map on the response observable to get the parsed people object
-      .map(res => res.json())
+    	.map(res => res.json())
       // Subscribe to the observable to get the parsed people object and attach it to the
       // component
-      .subscribe(people => this.people = people);
+      	.subscribe(people => this.people = people);
+		  var isNow = function(begin, end) {
+			  var currentDay = new Date();
+			  if ((currentDay.getHours() < (Number(begin[0]) - 1)))
+				  return false;
+			  if ((currentDay.getHours() == (Number(begin[0]) - 1)) && (currentDay.getMinutes() < (Number(begin[1]) - 1)))
+				  return false;
+			  if ((currentDay.getHours() > (Number(end[0]) - 1)))
+				  return false;
+			  if ((currentDay.getHours() == (Number(end[0]) - 1)) && (currentDay.getMinutes() > (Number(end[1]) - 1)))
+				  return false;
+			  return true;
+		  };
+		  var isCorrectDay = function(days) {
+			  var currentDay = new Date();
+			  for (var i = days.length - 1; i >= 0; i--) {
+				  if (days[i] == 'S' && currentDay.getDay() == 0)
+					  return true;
+				  if (days[i] == 'M' && currentDay.getDay() == 1)
+					  return true;
+				  if (days[i] == 'T' && currentDay.getDay() == 2)
+					  return true;
+				  if (days[i] == 'W' && currentDay.getDay() == 3)
+					  return true;
+				  if (days[i] == 'R' && currentDay.getDay() == 4)
+					  return true;
+				  if (days[i] == 'F' && currentDay.getDay() == 5)
+					  return true;
+			  }
+			  return false;
+		  };  
+		setInterval(function() {
+			var newPeople = []
+			for (var i = ctrl.people.length - 1; i >= 0; i--) {
+				for (var j = ctrl.people[i].hours.length - 1; j >= 0; j--) {
+					var normalized = ctrl.people[i].hours[j].time.replace(' ', '').replace('pm', '').replace('am', '').split('(')[0];
+					var correctTime = false;
+					for (var k = normalized.split(',').length - 1; k >= 0; k--) {
+						 var time = normalized.split(',')[k].split('-');
+						 var begin = time[0].split(':');
+						 var end = time[1].split(':');
+						 //convert to military time
+						 if(Number(begin[0]) < 9)
+							 begin[0] = (Number(begin[0]) + 12).toString();
+						 if (Number(end[0]) < 9)
+							 end[0] = (Number(end[0]) + 12).toString();
+						 correctTime = correctTime || (isNow(begin, end) && isCorrectDay(ctrl.people[i].hours[j].day.split('')));
+					}
+					if(correctTime)
+						newPeople.push(ctrl.people[i]);
+				}
+				ctrl.currentPeople = newPeople;
+			}
+		}, 3000);
+
   }
  }
