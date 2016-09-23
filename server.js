2d747875d605bf314311d632edc2855623500735
Jaiden Gerig <jaideng123@yahoo.com>
2016-09-23 10:01:10 -0500

    fixed port binding

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/', function(req,res){
 });
 
 app.use(express.static('.'));
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.ENV.PORT || 3000
+app.listen(port, function () {
+  console.log('Example app listening on port '+port);
 });
